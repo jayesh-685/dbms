126165e6f3de44ad4a0324a78377e2f856e53d75
Siddhant Sarkar <siddhant.sarkar999@gmail.com>
2022-05-07 00:02:40 +0530

    This is dope

--- comp_resolve.php
+++ comp_resolve.php
@@ -5,13 +5,11 @@ $feedback = $_POST["feedback"];
 $query = "select * from  `id18848798_jzclean`.`comp_table` where comlaintID = '$compID'";                                                                   
 $res= mysqli_query($con, $query);
 if($res){
-    // echo $query;
     $data = mysqli_fetch_array($res);
     if(json_encode($data) == 'null'){
         echo json_encode('complaint id not found') ;
         exit();
     }
-    // echo json_encode($data);
     $query1 = "INSERT INTO `resolved_comp` (`domain`, `room_no`, `time`, `description`, `comlaintID`, `feedback`) VALUES ('$data[0]', '$data[1]', '$data[2]', '$data[3]', '$data[4]', '$feedback');";
     $res1 = mysqli_query($con,$query1);
     $query2 = "delete from  `comp_table`  where comlaintID = '$compId';";
